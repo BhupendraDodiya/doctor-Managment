0e3b22bb0d102cd26ec99cf1ef1d394aa42c84c1
Bhupendra Dodiya <bdbhupendra123@gmail.com>
2023-03-02 12:33:53 +0530

    add choices for admin panel

--- doctor/dapp/models.py
+++ doctor/dapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Doctor(models.Model):
     Name = models.CharField(max_length=100)
     Degree = models.CharField(max_length=100)
@@ -8,11 +10,12 @@ class Doctor(models.Model):
     Email = models.EmailField()
     Password = models.CharField(max_length=100)
     Image = models.ImageField(upload_to='docimage/')
-    Category = models.CharField(max_length=100,choices=(('lungsSpecialist',"lungsSpecialist"),
-                                                        ('eyespecialist','eyespecialist'),
-                                                        ('heartspecialist','heartspecialist'),
-                                                        ('legspecialist','legspecialist'),
-                                                        ))
+    Category = models.CharField(max_length=100,choices=(('lungsSpecialist', "lungsSpecialist"),
+                  ('eyespecialist', 'eyespecialist'),
+                  ('heartspecialist', 'heartspecialist'),
+                  ('legspecialist', 'legspecialist'),
+                  ),default='eyespecialist')
+
 
 class User(models.Model):
     Name = models.CharField(max_length=100)
